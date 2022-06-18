6ea0eb69718124a67490f30789c0fb4cc24620df
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-06-18 13:04:13 +0000

    [Add]カラムの追加、コントローラー作成、View作成

--- db/migrate/20220618114256_devise_create_customers.rb
+++ db/migrate/20220618114256_devise_create_customers.rb
@@ -11,6 +11,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.string :postal_code,        null: false
+      t.string :address,            null: false
+      t.string :telephone_number,   null: false
+      t.boolean :is_active,         null: false, default: true
+
       ## Rememberable
       t.datetime :remember_created_at
 
